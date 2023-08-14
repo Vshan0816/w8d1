96f0d1d4ff1b5d31668daad37c0776669811a942
Vincent <vshan0816@gmail.com>
2023-08-14 15:43:29 -0400

    finish p02

--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -8,20 +8,32 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    prepare_render_or_redirect  
+    @res.status = 302
+    @res.location = url
+    nil
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    prepare_render_or_redirect  
+    @res.write(content)
+    @res.content_type = content_type
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
@@ -35,6 +47,11 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+  end
+
+  def prepare_render_or_redirect  
+    raise "double render error" if already_built_response?
+    @already_built_response = true
   end
 end
 
